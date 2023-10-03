6397896b326a929df923258263f61fb85e52a8e6
Samir Shakya <130730724+Samir16113@users.noreply.github.com>
2023-10-03 21:15:53 +0545

    Update and rename lab2qsn3b.cpp to lab2qsn3formatrixproduct.cpp

--- Ds lab2/lab2qsn3formatrixproduct.cpp
+++ Ds lab2/lab2qsn3formatrixproduct.cpp
@@ -2,82 +2,84 @@
 
 int main(void)
 {
-    
-    int rows1, columns1; // variables to store the number of rows and columns of matrix the user wants
+    int rows1, cols1;
     printf("For matrix A:\n");
     printf("Enter the rows: ");
     scanf("%d", &rows1);
     printf("Enter the columns: ");
-    scanf("%d", &columns1);
+    scanf("%d", &cols1);
 
-    
-    int rows2, columns2; // variables to store the number of rows and columns of matrix the user wants
-    printf("For matrix B:\n");
-    printf("Enter the rows: ");
-    scanf("%d", &rows2);
-    printf("Enter the columns: ");
-    scanf("%d", &columns2);
+    int A[rows1][cols1];
 
-    
-    int A[rows1][columns1], B[rows2][columns2], C[rows1][columns2];
-
-    
-    printf("\nEnter data for boolean matrix A:\n");
+    printf("\nEnter data for matrix A:\n");
     for (int i = 0; i < rows1; i++)
     {
-        for (int j = 0; j < columns1; j++)
+        for (int j = 0; j < cols1; j++)
         {
             printf("A[%d][%d]: ", i, j);
             scanf("%d", &A[i][j]);
         }
     }
 
-    
-    printf("\nEnter data for boolean matrix B:\n");
+    int rows2, cols2;
+    printf("\nFor matrix B:\n");
+    printf("Enter the rows: ");
+    scanf("%d", &rows2);
+    printf("Enter the columns: ");
+    scanf("%d", &cols2);
+
+    int B[rows2][cols2], C[rows1][cols2];
+
+    printf("\nEnter data for matrix B:\n");
     for (int i = 0; i < rows2; i++)
     {
-        for (int j = 0; j < columns2; j++)
+        for (int j = 0; j < cols2; j++)
         {
             printf("B[%d][%d]: ", i, j);
             scanf("%d", &B[i][j]);
         }
     }
 
-   
+    if (cols1 != rows2)
+    {
+        printf("Error: The number of columns in Matrix A must be equal to the number of rows in Matrix B for multiplication.\n");
+        return 1;
+    }
+
     printf("\nMatrix A:\n");
     for (int i = 0; i < rows1; i++)
     {
-        for (int j = 0; j < columns1; j++)
+        for (int j = 0; j < cols1; j++)
         {
             printf("%d\t", A[i][j]);
         }
         printf("\n");
     }
 
-    
     printf("Matrix B:\n");
     for (int i = 0; i < rows2; i++)
     {
-        for (int j = 0; j < columns2; j++)
+        for (int j = 0; j < cols2; j++)
         {
             printf("%d\t", B[i][j]);
         }
         printf("\n");
     }
 
-    printf("\nBoolean product of matrices A and B:\n");
+    printf("\nMatrix Product of matrices A and B:\n");
     for (int i = 0; i < rows1; i++)
     {
-        for (int j = 0; j < columns2; j++)
+        for (int j = 0; j < cols2; j++)
         {
             C[i][j] = 0;
-            for (int k = 0; k < rows2; k++)
+            for (int k = 0; k < cols1; k++)
             {
-                C[i][j] = C[i][j] || (A[i][k] && B[k][j]);
+                C[i][j] += A[i][k] * B[k][j];
             }
             printf("%d\t", C[i][j]);
         }
         printf("\n");
     }
+
     return 0;
 }
